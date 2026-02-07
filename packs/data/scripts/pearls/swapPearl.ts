08e2b3d8e2a01e2abe52a2a162ea74db1fa2573d
Mizab <mizabmansari@gmail.com>
2026-02-07 16:14:36 +0530

    Added the scripts for swap pearl

---
+++ packs/data/scripts/pearls/swapPearl.ts
@@ -0,0 +1,38 @@
+import { Entity, Vector3 } from "@minecraft/server";
+import constants from "../constants";
+import { BasePearl } from "./basePearl";
+import { getOwnerEntity } from "../utils/pearlUtils";
+
+export class SwapPearl extends BasePearl {
+  constructor() {
+    super(constants.SWAP_PEARL.PEARL_ID, constants.BUFFER_DISTANCE);
+  }
+
+  protected onImpact(pearl: Entity, impactLocation: Vector3) {
+    // Display Particle
+    const particleLocation = {
+      x: impactLocation.x,
+      y: impactLocation.y + 0.5,
+      z: impactLocation.z,
+    };
+    pearl.dimension.spawnParticle(constants.SWAP_PEARL.PARTICLE_NAME, particleLocation);
+
+    // Swap the player with pearl nearest player
+    const pearlLocation = pearl.location;
+    const owner = getOwnerEntity(pearl);
+    const ownerLocation = owner?.location;
+
+    const nearestEntity = pearl.dimension.getEntities({
+      location: pearlLocation,
+      minDistance: constants.SWAP_PEARL.MIN_DISTANCE,
+      maxDistance: constants.SWAP_PEARL.IMPACT_DISTANCE,
+      excludeFamilies: ["inanimate"],
+      closest: 1,
+    })[0];
+
+    if (nearestEntity && ownerLocation && owner) {
+      owner.teleport(pearlLocation);
+      nearestEntity.teleport(ownerLocation);
+    }
+  }
+}
